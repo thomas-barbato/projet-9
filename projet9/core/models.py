ffd39230a1e9af9009f64c2baeb42a685d27bc61
Case Gibson <t.barbato78@gmail.com>
2023-01-15 14:22:02 +0100

    first push, test driven added.

---
+++ projet9/core/models.py
@@ -0,0 +1,33 @@
+import datetime
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projet9.settings")
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField('Created Time', auto_now_add=True)
+
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    followed_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followed_by")
+
+    class Meta:
+        unique_together = ('user', 'followed_user')
+
+
+class Review(models.Model):
+    # need to search for a solution about null=True in FK...
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    time_created = models.DateTimeField('Created Time', auto_now_add=True)
---
+++ projet9/core/tests.py
@@ -0,0 +1,86 @@
+from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
+import datetime
+
+# Create your tests here.
+class ReviewTest(TestCase):
+    # this fun start everytime
+    # i made a test.
+    # can be used to
+    # create fake user (eg)
+    def setUp(self):
+        print("hello setUp")
+        User.objects.create(
+            username="test",
+            password="passwordtest123*",
+            email="email@email.com",
+        )
+        u = User.objects.get(username="test")
+
+        Ticket.objects.create(
+            title="un titre",
+            description="une description",
+            time_created=datetime.datetime.now(),
+            user_id = 1,
+        )
+        t = Ticket.objects.get(title="un titre")
+
+        Review.objects.create(
+            ticket_id=t.id,
+            rating=4,
+            user_id=u.id,
+            headline="Un titre",
+            body="un corps de text",
+            time_created=datetime.datetime.now()
+        )
+        UserFollows.objects.create(
+            followed_user_id=u.id,
+            user_id=u.id
+        )
+    # fun executed at the end of
+    # a test
+    def tearDown(self):
+        print("hello tearDown")
+
+    def test_user_success(self):
+        assert User.objects.count() == 1
+        print("user success")
+        self.assertTrue(True)
+
+    def test_user_fail(self):
+        try:
+            u = User(name='1234')
+        except Exception as e:
+            print(f"user fail, {e}")
+
+    def test_ticket_success(self):
+        print("ticket success")
+        assert Ticket.objects.count() == 1
+        self.assertTrue(True)
+
+    def test_ticket_fail(self):
+        try:
+            Ticket.objects.create(title=None)
+        except Exception as e:
+            print(f"ticket fail, {e}")
+
+    def test_review_success(self):
+        print("review success")
+        assert Review.objects.count() == 1
+        self.assertTrue(True)
+
+    def test_review_fail(self):
+        try:
+            Review(
+                rating="",
+                headline="Test headline",
+                body="Empty body"
+            ).save()
+        except Exception as e:
+            print(f"review fail, {e}")
+
+    def test_userFollow_success(self):
+        print("userFollow success")
+        assert UserFollows.objects.count() == 1
+        self.assertTrue(True)
