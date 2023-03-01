e173b86a1682ef1c6be211880334981d9038696c
Case Gibson <t.barbato78@gmail.com>
2023-03-01 16:26:46 +0100

    Border (red / greed) added in create_user template js

--- core/static/js/scripts/create_user.js
+++ core/static/js/scripts/create_user.js
@@ -4,6 +4,10 @@
         window.location.href = login_view;
     });
 
+    $('form>input').on('change', function(){
+        $(this).css('border','');
+    })
+
     $(document).on('keypress',function(e) {
         if(e.which == 13) {
             $('.signup-button').click()
@@ -31,17 +35,34 @@
                 }else{
                     let password_elem = $('#password_error')
                     let username_elem = $('#username_error')
-                    $.each(json.errors, function( index, value ) {
-                      if(index === "password" || index === "password2" || index === "__all__"){
-                        password_elem.html(value);
-                        password_elem.show().fadeOut(5000);
-                      }else if(index === "username"){
-                        username_elem.html(value);
-                        username_elem.show().fadeOut(5000);
-                      }
+                    let index_error = ['password', 'password2', '__all__', 'username']
+                    let array = $.each(json.errors, function(key, value){
+                        return [key, value]
+                    })
+                    $.each(index_error, function(index){
+                        if(index_error[index] in array){
+                            if(index_error[index] == "password" || index_error[index] == "password2"){
+                               $('#id_password, #id_password2').css('border', '2px solid red');
+                                $(password_elem).html(array["password2"]).show().fadeOut(5000);
+                            }else if(index_error[index] == "__all__"){
+                                $('form>input').css('border', '2px solid red');
+                                $(password_elem).html(array["__all__"]).show().fadeOut(5000);
+                            }else if(index_error[index] == "username"){
+                                $("#id_username").css('border', '2px solid red');
+                                $(username_elem).html(array["username"]).show().fadeOut(5000);
+                            }
+                        }else{
+                            $('#id_'+index_error[index]).css('border', '2px solid green');
+                        }
+                    })
+                    $(document).ready(function () {
+                        setTimeout( function(){
+                            $('form>input').css('border','');
+                        },5000);
                     });
+
                 }
-            },
+            }
 
         })
     })
